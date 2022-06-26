b154a02f7ea4910c80fadf31e900c4722c84e4a0
pachaBauer <eabestudio@gmail.com>
2022-06-26 20:32:58 -0300

    agregue: crear cuadricula de div y hover

--- code.js
+++ code.js
@@ -1,14 +1,30 @@
 const body = document.querySelector('body');
-const div = document.createElement('div');
-body.appendChild(div);
-div.classList.add('items');
+const div1 = document.createElement('div');
+body.appendChild(div1);
+div1.classList.add('items');
 
-for (let i = 1; i <= 16; i++) { /* aqui luego definir el i como un maximo determinado x el usuario */
-    const items = document.querySelector('.items');
-    const div = document.createElement('div');
-    items.appendChild(div);
-    div.classList.add('item')
-    div.textContent = i;   
-}
+const items = document.querySelector('.items')
+function createGrid(rows,cols){
+    items.style.setProperty('--grid-rows',rows);
+    items.style.setProperty('--grid-cols',cols);
+    for (c=0; c < (rows * cols); c++) {
+        let cell = document.createElement('div');
+        cell.textContent = c + 1;
+        items.appendChild(cell).className = "item";
+    };
+};
+createGrid(2,2); // here i could relate the prompt
+
+let color = "#3CE746"; /* aqui defino la variable color que puede ir asociada a seleccionar el color */
+
+const item = document.querySelectorAll('.item');
+item.forEach((i) => {
+    i.addEventListener('mouseenter', function() {
+    i.style.background = color;
+    });
+});
+
+
+
 
 
