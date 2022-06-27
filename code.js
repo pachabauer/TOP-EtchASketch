f3f62dbbfc71db540cf562a29a9cfdf40874bb0a
pachaBauer <eabestudio@gmail.com>
2022-06-26 21:41:24 -0300

    3er commit hover and color ok

--- code.js
+++ code.js
@@ -1,30 +1,27 @@
-const body = document.querySelector('body');
-const div1 = document.createElement('div');
-body.appendChild(div1);
-div1.classList.add('items');
+const body = document.querySelector("body");
+const div3 = document.createElement("div");
+body.appendChild(div3);
+div3.classList.add("items");
 
-const items = document.querySelector('.items')
-function createGrid(rows,cols){
-    items.style.setProperty('--grid-rows',rows);
-    items.style.setProperty('--grid-cols',cols);
-    for (c=0; c < (rows * cols); c++) {
-        let cell = document.createElement('div');
-        cell.textContent = c + 1;
-        items.appendChild(cell).className = "item";
-    };
-};
-createGrid(2,2); // here i could relate the prompt
+// create the grid
+const items = document.querySelector(".items");
+function createGrid(rows, cols) {
+  items.style.setProperty("--grid-rows", rows);
+  items.style.setProperty("--grid-cols", cols);
+  for (c = 0; c < rows * cols; c++) {
+    let cell = document.createElement("div");
+    items.appendChild(cell).className = "item";
+  }
+}
+createGrid(5, 5); // here i could relate the prompt
 
-let color = "#3CE746"; /* aqui defino la variable color que puede ir asociada a seleccionar el color */
-
-const item = document.querySelectorAll('.item');
+// create the color picker
+let colorInput = document.querySelector("#color"); 
+let hexInput = document.querySelector("#hex");
+const item = document.querySelectorAll(".item");
 item.forEach((i) => {
-    i.addEventListener('mouseenter', function() {
+  i.addEventListener("mouseenter", function () {
+    let color = colorInput.value;
     i.style.background = color;
-    });
+  });
 });
-
-
-
-
-
