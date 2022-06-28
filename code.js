c7404dab731f7c84da78c288ccc836ad3f4194fa
pachaB <eabestudio@gmail.com>
2022-06-28 12:48:54 -0300

    4- css changes

--- code.js
+++ code.js
@@ -1,7 +1,38 @@
+// variable declares
 const body = document.querySelector("body");
+const title = document.createElement("div");
+
+const buttons = document.createElement("div");
+const p1 = document.createElement("p");
+const black  =document.createElement('button');
+const rainbow  =document.createElement('button');
+const clear  =document.createElement('button');
+
 const div3 = document.createElement("div");
-body.appendChild(div3);
+const inputColorButton = document.createElement("input");
+
+
+// variable add classes
+title.classList.add('titulo');
+buttons.classList.add('buttons')
+p1.classList.add('p1Color');
 div3.classList.add("items");
+black.classList.add("black");
+rainbow.classList.add("rainbow");
+clear.classList.add("clear");
+
+// variable set attributes
+title.textContent = "Etch a Sketch";
+inputColorButton.type = "color";
+inputColorButton.setAttribute("id","color");
+p1.textContent = "Pick a color";
+black.textContent = "Just Black"
+rainbow.textContent = "Rainbow"
+clear.textContent = "Reset";
+
+// append
+buttons.append(inputColorButton,p1,black,rainbow,clear);
+body.append(title,buttons,div3);
 
 // create the grid
 const items = document.querySelector(".items");
